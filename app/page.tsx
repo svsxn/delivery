3a1ac60858be1339d2fbbeee53a1068d1ead4af9
svsxn <sebmagn102@gmail.com>
2026-04-29 19:14:01 +0200

    feat: add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-neutral-700 sm:items-start">
       Home
     </div>
   );
