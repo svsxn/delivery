2dde4230f8de6a2373717f3ebce4cf3f231e0a8d
svsxn <sebmagn102@gmail.com>
2026-04-30 17:15:43 +0200

    feat: add example chart

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,13 +6,13 @@ export default async function Navbar() {
   return (
     <nav className="bg-black h-16 flex justify-center text-white text-md">
       <div className="flex items-center justify-center bg-neutral-900 h-12 w-16 rounded-bl-xl">
-        <span>43.23zł</span>
+        <span>0.00zł</span>
       </div>
       <div className="flex items-center justify-center bg-neutral-800 h-full w-20 rounded-b-2xl">
         <span className="text-xl">{data}zł</span>
       </div>
       <div className="flex items-center justify-center bg-neutral-900 h-12 w-16 rounded-br-xl">
-        <span>300.23zł</span>
+        <span>0.00zł</span>
       </div>
     </nav>
   );
