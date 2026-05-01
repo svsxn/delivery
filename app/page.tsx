dc3805938395fdc15ebd12d9f49eaadc96dc522b
svsxn <sebmagn102@gmail.com>
2026-05-02 00:01:13 +0200

    feat: add weekly chart

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { ChartExample } from "./_components/chart";
+import { WeeklyChart } from "./_components/weekly_chart";
 
 export default function Home() {
   return (
     <div className="flex flex-1 w-full flex-col items-center gap-6 py-6 px-16 sm:items-start">
       <div className="flex w-full justify-between gap-4 h-60">
-        <div className="bg-neutral-900 rounded-xl w-full border border-neutral-800"></div>
+        <WeeklyChart />
+        {/* <div className="bg-neutral-900 rounded-xl w-full border border-neutral-800"></div> */}
         <div className="bg-neutral-900 rounded-xl w-full border border-neutral-800"></div>
         <div className="bg-neutral-900 rounded-xl w-full border border-neutral-800"></div>
         {/* <div className="bg-neutral-900 rounded-xl h-60 w-full"></div> */}
